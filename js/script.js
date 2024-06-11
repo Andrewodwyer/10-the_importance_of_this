1d2cee03ac97d6c50cc115e81aa59b4f4c026cd4
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-06-11 13:07:04 +0000

    boxes to read by class number

--- js/script.js
+++ js/script.js
@@ -6,9 +6,13 @@
 // 	$(this).removeClass('makeBlue').addClass('makeRed');
 // });
 
-$(document).ready(function(){
-    $(".box").on("click", function(){
-        var claasNames = $(this).attr("class").split(" ");
+$(document).ready(function() {
+    $(".box").on("click", function() {
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
         $("." + classNames[1]).css("background-color", "red");
     });
 });
